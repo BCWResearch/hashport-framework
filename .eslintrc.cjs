68c9c85668fd7aab34c85f762af2b95378a381d9
john-bcw <john@bcw.group>
2023-04-28 15:07:36 +0900

    prettier init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,16 @@
 module.exports = {
   env: {
     browser: true,
-    es2020: true
+    es2020: true,
+    node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
+    'prettier'
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
