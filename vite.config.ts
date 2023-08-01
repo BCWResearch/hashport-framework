eec0c0632775f3c675386c0337bbbc61f026458c
john-bcw <john@bcw.group>
2023-08-01 12:18:55 +0900

    polyfill buffer for hashgraph sdk

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { polyfillNode } from 'esbuild-plugin-polyfill-node';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    optimizeDeps: {
+        esbuildOptions: {
+            plugins: [polyfillNode({ globals: { process: false } })],
+        },
+    },
 });
