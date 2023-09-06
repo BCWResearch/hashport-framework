d43c6b927b68d65a4ffeca9f0c2dad21dc865db5
john-bcw <john@bcw.group>
2023-09-06 13:59:57 +0900

    responsive pass on acct btns

--- packages/widget/src/components/HashportWidget/AccountButton.tsx
+++ packages/widget/src/components/HashportWidget/AccountButton.tsx
@@ -3,14 +3,23 @@ import { styled } from '@mui/material/styles';
 import { ButtonProps } from '@mui/material/Button';
 import Avatar from 'boring-avatars';
 
-export const StyledButton = styled(Button)(({ theme: { palette }, variant }) => ({
-    fontFamily: ['Inter', '-apple-system', 'Segoe UI', 'Helvetica', 'sans-serif', 'monospace'],
-    color: 'white',
-    backgroundColor: variant === 'contained' ? palette.primary.dark : 'initial',
-    '&:hover': {
-        backgroundColor: palette.primary.main,
-    },
-}));
+export const StyledButton = styled(Button)(
+    ({ theme: { palette, breakpoints, spacing }, variant }) => ({
+        fontFamily: ['Inter', '-apple-system', 'Segoe UI', 'Helvetica', 'sans-serif', 'monospace'],
+        color: 'white',
+        backgroundColor: variant === 'contained' ? palette.primary.dark : 'initial',
+        '&:hover': {
+            backgroundColor: palette.primary.main,
+        },
+        [breakpoints.down(400)]: {
+            padding: spacing(0.5, 0.75),
+            flexWrap: 'wrap-reverse',
+            gap: spacing(1),
+            overflowWrap: 'anywhere',
+            '& .MuiButton-startIcon': { marginRight: 0 },
+        },
+    }),
+);
 
 export const AccountButton = ({ account, ...buttonProps }: ButtonProps & { account?: string }) => {
     return (
